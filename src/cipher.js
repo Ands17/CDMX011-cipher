297511de6e72fadbd446c9ba8d58d67ab9463fff
Andrea Herrera <ands.he09@gmail.com>
2021-07-14 22:38:53 -0500

    avance del proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,58 @@
+
+
 const cipher = {
-  // test
-};
+  encode: function(offset, string){
+     if( typeof string!="string"){
+      throw TypeError('Valores incorrectos');
+      
+    } 
+    
+   
+  let newCifrado = ""
+  let cifrado = ""
+ 
+  for (let i = 0; i < string.length; i++) {
+    let textAscci = string.charCodeAt(i)
+    if(textAscci>= 65 && textAscci <=90){
+     cifrado = ((textAscci - 65 + parseInt(offset)) % 26 + 65)}
+    else if (textAscci >= 97 && textAscci <= 122){
+      cifrado = ((textAscci - 97 + parseInt(offset)) % 26 + 97)}
+
+   newCifrado += String.fromCharCode(cifrado)
+    
+  }
+ 
+ return newCifrado
+
+},
+
+decode: function(offset, string) {
+ if( typeof string!="string"){
+    throw TypeError('Valores incorrectos');
+  } 
+
+  let newDecifrado = ""
+  let textDecifrado =""
+
+  for (let i = 0; i < string.length; i++) {
+     let textAscci = string.charCodeAt(i);
+    if(textAscci>= 65 && textAscci <=90){
+    textDecifrado = ((textAscci + 65 - parseInt(offset)) % 26 + 65)}
+    else if (textAscci >= 97 && textAscci <= 122) {
+      textDecifrado = ((textAscci - 97 - parseInt(offset)) % 26 + 97)}
+    newDecifrado += String.fromCharCode(textDecifrado)
+  }
+
+  return newDecifrado
+
+  
+  }
+
+
+
+
+
+}; 
+
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
-import cipher from './cipher.js';
 
-console.log(cipher);
+import cipher from './cipher.js';
+//import {encode, decode} from'./cipher.js';
+
+//Invocar boton encriptar
+const btnEncriptar = document.getElementById("encriptar");
+btnEncriptar.addEventListener("click", function () {
+
+let offset = document.getElementById("desplazamiento").value;
+let string = document.getElementById("mensaje").value;
+let newCifrado = cipher.encode(offset, string);
+ document.getElementById ("resultado"). value = newCifrado
+
+});
+
+//invocar boton desencriptar
+const btnDecifrar = document.getElementById("decifrar");
+btnDecifrar.addEventListener("click", mostrarMensaje2);
+function mostrarMensaje2 (){
+let offset = document.getElementById("desplazamiento").value;
+let string = document.getElementById("mensaje").value;
+let newDecifrado = cipher.decode (offset, string);
+
+document.getElementById ("resultado"). value= newDecifrado
+
+}
+
+//Boton Borrar
+const btnBorrar = document.getElementById ("borrar")
+btnBorrar.addEventListener("click", borrar);
+function borrar() {
+    document.getElementById("mensaje").value= ""
+	document.getElementById ("resultado"). value= ""
+	document.getElementById("desplazamiento").value = ""
+}
+
+//Boton copiar
+const btnCopiar = document.getElementById ("copiar")
+btnCopiar.addEventListener("click", copiar);
+function copiar() {
+	let contenido = document.getElementById ("resultado")
+	contenido.select()
+    document.execCommand('copy');
+
+}
+
+
+
+
+
